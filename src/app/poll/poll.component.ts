ae96285d82f83821d34ec355f7721b8956613573
Jakub Bartak <kubabartak@yahoo.com>
2017-06-18 16:44:16 +0200

    charts added

--- src/app/poll/poll.component.ts
+++ src/app/poll/poll.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { PollService} from '../services/poll.service';
 import {Polls} from '../../Poll';
+import {ChartsModule} from 'ng2-charts';
 
 
 @Component({
@@ -18,7 +19,23 @@ poll_options: string[];
 newAnswers: string[];
 Id: string;
 VoteValue;
+chartLabels: any =[];
+chartData: number[]=[];
+// Pie
 
+
+  public pieChartType:string = 'pie';
+  public pieChartOptions: any = {responsive: true, 
+  	maintainAspectRatio: false};
+ 
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+ 
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
 
     constructor(private route: ActivatedRoute,
     	private pollService:PollService) {
@@ -32,27 +49,48 @@ VoteValue;
 		.subscribe(poll =>{
 			this.poll_name = poll.poll_name;
 			this.poll_options = poll.poll_options;
+			console.log(poll.poll_options);
+			this.chartLabels=[];
+			for (let i of poll.poll_options) {
+				
+				this.chartData.push(i.answer_vote.length);
+				this.chartLabels.push(i.answer_name);
+				
+			};
+			console.log(this.chartLabels)
 			});			
 		}
 
 		addAnswers(event){
+			
 		event.preventDefault();
 		this.pollService.addAnswers(this.newAnswers, this.Id)
-		.subscribe(poll =>{
-			this.poll_options=poll.poll_options;
+		.subscribe(updated =>{
+			this.poll_options=updated.poll_options;
 			this.newAnswers=[];	
+			this.chartLabels=[];
+			this.chartData=[];
+			for (let i of updated.poll_options) {
+				this.chartLabels.push(i.answer_name);
+				this.chartData.push(i.answer_vote.length)	
+			};
+			console.log(this.chartLabels)
 		})
+		
     }
 
 		addVote(event){
+			
 		event.preventDefault();
 		this.pollService.addVote(this.VoteValue, this.Id)
 		.subscribe(docs =>{
-			
-			this.poll_options=docs.poll_options
-			
-			;
-			
+			this.chartData=[];
+			this.poll_options=docs.poll_options;
+			for (let i of docs.poll_options) {
+				
+				this.chartData.push(i.answer_vote.length)
+			};
+			console.log(this.chartData)
 		})
     };
 
